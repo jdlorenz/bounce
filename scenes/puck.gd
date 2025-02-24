9ef2c1e954e05d3ce72ec984cc4ba0577635635f
jdlorenz <jonathondlorenz@gmail.com>
2025-02-24 17:25:37 -0500

    adding in base player / puck changes
    adding in base player / puck changes

--- scenes/puck.gd
+++ scenes/puck.gd
@@ -6,3 +6,6 @@ func _physics_process(delta):
 	var collision_info = move_and_collide(ball_speed * delta)
 	if collision_info:
 		ball_speed = ball_speed.bounce(collision_info.get_normal())
+
+func _process(delta):
+	rotate(5 * delta)
