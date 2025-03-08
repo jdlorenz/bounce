0a89665a823275a74a788553d505222814b67acb
jdlorenz <jonathondlorenz@gmail.com>
2025-03-08 08:49:41 -0500

    speed decay

--- scenes/puck.gd
+++ scenes/puck.gd
@@ -6,16 +6,18 @@ var ball_vector = Vector2(ball_speed * cos(deg_to_rad(ball_angle)),ball_speed * 
 var last_collision_id = 0
 
 func _friction(delta):
-	if ball_speed > 100.0:
-		ball_speed = ball_speed - 1
+	if ball_speed > 150.0:
+		ball_speed = ball_speed *.99
+	if ball_speed < 150.0:
+		ball_speed = 150.0
 
 func _physics_process(delta):
 	_friction(delta)
-	print(ball_vector)
 	mass = 100
 	var collision_info = move_and_collide(ball_vector * delta)
 	if collision_info:
 		var collider = collision_info.get_collider()
+		print(collider.get_instance_id())
 		if last_collision_id == collider.get_instance_id() and collider.get_class() == "RigidBody2D":
 			pass
 		elif collision_info.get_collider().get_class() == "RigidBody2D":
